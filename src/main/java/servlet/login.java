37b1f3bc8de99aca8b099e92bc3a9fa8829a3729
246031-blip <246031@std.hi-joho.ac.jp>
2026-06-04 10:28:29 +0900

    ログイン機能

--- src/main/java/servlet/login.java
+++ src/main/java/servlet/login.java
@@ -2,38 +2,42 @@ package servlet;
 
 import java.io.IOException;
 
+import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
 
-/**
- * Servlet implementation class login
- */
+@WebServlet("/Login")
 public class login extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
     public login() {
         super();
-        // TODO Auto-generated constructor stub
     }
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		
 	}
-
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
+	
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+		
+		request.setCharacterEncoding("UTF-8");	
+		String id = request.getParameter("id");
+		String pass = request.getParameter("pass");
+		
+		User user = new User(id,pass);
+		
+		Loginlogic loginlogic = new LoginLogic();
+		boolean isLogin = loginLogic.execute(user);
+		
+		if(isLogin) {
+			HttpSession session = request.getSession();
+			session.setAttribute("loginUser",user);
+			}
+		RequestDispatcher dispacher = request.getRequestDispatcher("WEB-INF/jsp/loginResult.jsp");
+		dispacher.forward(request,response);
 	}
 }
