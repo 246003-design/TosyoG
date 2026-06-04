fd88d50c553d1736ab82383a61927ae02476f5c1
habararyuki <246089@std.hi-joho.ac.jp>
2026-06-04 10:43:40 +0900

    現在の利用者区分でアクセスできるか判定

---
+++ src/main/java/servlet/RentalStatusServlet.java
@@ -0,0 +1,37 @@
+//貸出状況照会
+
+package servlet;
+
+import java.io.IOException;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+
+public class RentalStatusServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+
+    public RentalStatusServlet() {
+        super();
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
