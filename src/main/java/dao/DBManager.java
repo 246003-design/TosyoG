46ecc7e15521b72e6599a2c248d8cb1aec779069
habararyuki <246089@std.hi-joho.ac.jp>
2026-06-09 14:42:51 +0900

    DriverManager完成

--- src/main/java/dao/DBManager.java
+++ src/main/java/dao/DBManager.java
@@ -2,11 +2,15 @@ package dao;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class DBManager {
 	public static Connection getConnection() {
 		
 		String jdbcDriver = "com.mysql.cj.jdbc.Driver";
+		String url = "jdbc:mysql://localhost:3306/s3a1_systm_dev";
+		String user = "root";
+		String password = "password";
 		
 		try {
 			Class.forName(jdbcDriver);
@@ -14,6 +18,16 @@ public class DBManager {
 			throw new IllegalStateException("JDBCドライバを読み込めませんでした。");
 		}
 		
-		return "jdbc:mysql://localhost:3306/あなたのデータベース名"
+		Connection conn;
+		
+		try{
+			conn = DriverManager.getConnection(url, user, password);
+				
+			
+		} catch (SQLException e) {
+			throw new IllegalStateException("sqlにアクセスできませんでした。");
+		}
+		
+		return conn;
 	}
 }
