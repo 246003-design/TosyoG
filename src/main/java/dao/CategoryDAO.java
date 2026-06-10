6086b6169743bc274e85ad9318b707dd31d250c2
246089 <246089@WINDOWS-J895ARQ.mr.hi-joho.ac.jp>
2026-06-10 14:09:28 +0900

    CategoryDAO完成　id検索、全取得を作成

--- src/main/java/dao/CategoryDAO.java
+++ src/main/java/dao/CategoryDAO.java
@@ -4,6 +4,8 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 import entity.Category;
 
@@ -17,7 +19,6 @@ public class CategoryDAO extends BaseDAO{
 	
 	public Category findById(int id){
 		Category category = null;
-		category = null;
 		
 		String sql = "SELECT * FROM category WHERE id = ? AND deleted_at IS NULL";
 		try (PreparedStatement pstms = conn.prepareStatement(sql)){
@@ -26,6 +27,7 @@ public class CategoryDAO extends BaseDAO{
 			try(ResultSet rs = pstms.executeQuery()){
 				if(rs.next()) {
 					category = new Category();
+					
 					category.setId(rs.getInt("id"));
 					category.setName(rs.getString("name"));
 					category.setSortOrder(rs.getInt("sort_order"));
@@ -41,5 +43,29 @@ public class CategoryDAO extends BaseDAO{
 		return category;
 	}
 	
-	public
+	public List<Category>findAll(){
+		List<Category> list = new ArrayList<>();
+		
+		String sql = "SELECT * FROM category WHERE deleted_at IS NULL ORDER BY sort_order ASC";
+		
+		try(PreparedStatement pstmt = conn.prepareStatement(sql);
+				ResultSet rs = pstmt.executeQuery()){
+			
+			while(rs.next()) {
+				Category category = new Category();
+				
+				category.setId(rs.getInt("id"));
+				category.setName(rs.getString("name"));
+				category.setSortOrder(rs.getInt("sort_order"));
+				category.setCreatedAt(rs.getTimestamp("created_at"));
+				category.setUpdatedAt(rs.getTimestamp("updated_at"));
+				category.setDeletedAt(rs.getTimestamp("deleted_at"));
+                
+                list.add(category);
+			}
+		}catch(SQLException e) {
+			e.printStackTrace();
+		}
+		return list;
+	}
 }
