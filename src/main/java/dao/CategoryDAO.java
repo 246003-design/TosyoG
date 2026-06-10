2f0c8be742336b3618ba8ef47508c7d50c7246f6
habararyuki <246089@std.hi-joho.ac.jp>
2026-06-10 10:40:41 +0900

    主キーで調べれるように

--- src/main/java/dao/CategoryDAO.java
+++ src/main/java/dao/CategoryDAO.java
@@ -1,12 +1,43 @@
-//継承してdaoを作ってください。
 package dao;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import entity.Category;
 
 public class CategoryDAO extends BaseDAO{
 
 	public CategoryDAO(Connection conn) {
 		super(conn);
 	}
+	
 
+	
+	public Category findById(int id){
+		Category category = null;
+		category = null;
+		
+		String sql = "SELECT * FROM category WHERE id = ? AND deleted_at IS NULL";
+		try (PreparedStatement pstms = conn.prepareStatement(sql)){
+			pstms.setInt(1, id);
+			
+			try(ResultSet rs = pstms.executeQuery()){
+				if(rs.next()) {
+					category = new Category();
+					category.setId(rs.getInt("id"));
+					category.setName(rs.getString("name"));
+					category.setSortOrder(rs.getInt("sort_order"));
+					category.setCreatedAt(rs.getTimestamp("created_at"));
+					category.setUpdatedAt(rs.getTimestamp("updated_at"));
+					category.setDeletedAt(rs.getTimestamp("deleted_at"));
+				}
+			}
+		}catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
+		return category;
+	}
 }
