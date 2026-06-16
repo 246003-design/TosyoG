254827c8b75ed2c40af92aa7e8c5c998660f6e8f
takeda246037 <246037@std.hi-joho.ac.jp>
2026-06-16 15:22:47 +0900

    これも

--- src/main/java/dao/AuthorDAO.java
+++ src/main/java/dao/AuthorDAO.java
@@ -9,66 +9,62 @@ import java.util.List;
 import java.util.Optional;
 
 import entity.Author;
-//著者DAO
-public class AuthorDAO extends BaseDAO{
+
+// 著者DAO
+public class AuthorDAO extends BaseDAO {
 
 	public AuthorDAO(Connection conn) {
 		super(conn);
-		
 	}
-//著者IDから著者情報を取得
-	public Optional<Author>findIById(int id){
+
+	// 著者IDから著者情報を取得
+	public Optional<Author> findById(int id) {
+		String sql = "SELECT id, name, created_at, updated_at, deleted_at FROM author WHERE id = ? AND deleted_at IS NULL";
 		
-		String sql ="SELECT id,name,createdAt,updatedAt,deletedAt"
-					+"FROM author WEHER id =?　AND deledAt IS NULL";
-		
-		try(PreparedStatement pstmt = conn.prepareStatement(sql)){
+		try (PreparedStatement pstmt = conn.prepareStatement(sql)) {
 			pstmt.setInt(1, id);
 			
-			try(ResultSet rs = pstmt.executeQuery()){
-				
-				if(rs.next()) {
+			try (ResultSet rs = pstmt.executeQuery()) {
+				if (rs.next()) {
 					Author author = new Author();
 					author.setId(rs.getInt("id"));
 					author.setName(rs.getString("name"));
-					author.setCreatedAt(rs.getTimestamp("createAt"));
-					author.setUpdatedAt(rs.getTimestamp("updatedAt"));
-					author.setDeletedAt(rs.getTimestamp("deletedAt"));
+					author.setCreatedAt(rs.getTimestamp("created_at"));
+					author.setUpdatedAt(rs.getTimestamp("updated_at"));
+					author.setDeletedAt(rs.getTimestamp("deleted_at"));
 					
 					return Optional.of(author);
 				}
 			}
-		}catch (SQLException e) {
-			System.err.println("findByIdの実行中にエラーが発生しました。");
+		} catch (SQLException e) {
+			System.err.println("AuthorDAO.findByIdの実行中にエラーが発生しました。");
 			e.printStackTrace();
 		}
 		return Optional.empty();
 	}
 	
-//著者テーブルの全情報を取得する
-	public List<Author>findAll(){
-		List<Author>list = new ArrayList<>();
+	// 著者テーブルの全情報を取得する
+	public List<Author> findAll() {
+		List<Author> list = new ArrayList<>();
+		String sql = "SELECT id, name, created_at, updated_at, deleted_at FROM author WHERE deleted_at IS NULL";
 		
-		String sql = "SELECT id,name,createdAt,updatedAt,deletedAt"
-				+"FROM author WEHER deledAt IS NULL";
-		
-		try(PreparedStatement pstmt = conn.prepareStatement(sql);
-				ResultSet rs = pstmt.executeQuery()){
+		try (PreparedStatement pstmt = conn.prepareStatement(sql);
+				ResultSet rs = pstmt.executeQuery()) {
 			
-			while(rs.next()) {
+			while (rs.next()) {
 				Author author = new Author();
 				author.setId(rs.getInt("id"));
 				author.setName(rs.getString("name"));
-				author.setCreatedAt(rs.getTimestamp("createAt"));
-				author.setUpdatedAt(rs.getTimestamp("updatedAt"));
-				author.setDeletedAt(rs.getTimestamp("deletedAt"));
+				author.setCreatedAt(rs.getTimestamp("created_at"));
+				author.setUpdatedAt(rs.getTimestamp("updated_at"));
+				author.setDeletedAt(rs.getTimestamp("deleted_at"));
 				
 				list.add(author);
 			}
-	}catch(SQLException e) {
-		System.err.println("findAllの実行中にエラーが発生しました。");
-		e.printStackTrace();
-	}
+		} catch (SQLException e) {
+			System.err.println("AuthorDAO.findAllの実行中にエラーが発生しました。");
+			e.printStackTrace();
+		}
 		return list;
 	}
 }
