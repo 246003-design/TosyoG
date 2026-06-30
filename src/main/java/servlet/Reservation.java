a242d1bf3c9c79fcae47f13c07fe88157bac4bed
246031-blip <246031@std.hi-joho.ac.jp>
2026-06-30 13:48:24 +0900

    修正

--- src/main/java/servlet/Reservation.java
+++ src/main/java/servlet/Reservation.java
@@ -28,7 +28,7 @@ public class Reservation extends HttpServlet {
         Integer userId = (Integer) session.getAttribute("loginUserId"); 
 
         if (userId == null) {
-            response.sendRedirect(request.getContextPath() + "WEB-INF/common/JSP/login.jsp");
+            response.sendRedirect(request.getContextPath() + "/WEB-INF/JSP/JSP/login.jsp");
             return;
         }
 
