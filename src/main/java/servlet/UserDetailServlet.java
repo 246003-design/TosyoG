116628f0cdaf7b034869d6d4509a8f678f82578a
246089 <246089@WINDOWS-L2OQITA.mr.hi-joho.ac.jp>
2026-07-02 12:26:19 +0900

    利用者情報更新画面作成中

---
+++ src/main/java/servlet/UserDetailServlet.java
@@ -0,0 +1,55 @@
+package servlet;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import dao.DBManager;
+import dao.UserDAO;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class UserDetailServlet
+ */
+public class UserDetailServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public UserDetailServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		int id = request.getParameter("userId");
+		
+		
+		try(Connection conn = DBManager.getConnection()){
+			UserDAO dao = new UserDAO(conn);
+			
+			dao.findById(id);
+			
+			request.setAttribute("userList", dtoList);
+			request.getRequestDispatcher("/WEB-INF/JSP/admin/admin_user_list.jsp").forward(request, response);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
